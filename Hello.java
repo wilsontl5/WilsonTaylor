b52db7c1075d3b3bc92b1c03a79a5adb3a8c38eb
wilsontl5 <59926354+wilsontl5@users.noreply.github.com>
2020-01-15 10:26:53 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/**********************************
+* Hello.java
+***********************************
+* Taylor Wilson
+* 1/15/2020
+* CMSC 255-003
+***********************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Taylor Wilson");
+		System.out.println("Hello.java");
+		System.out.println("Programming to print my name");
+		System.out.println("1/15/2020");
+		System.out.println("CMSC 255-003");
+	}
+}
